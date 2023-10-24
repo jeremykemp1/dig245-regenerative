9d5cac673c88e20feae5537e5713973123f489fb
Jeremy Kemp <jekemp@davidson.edu>
2023-10-24 17:54:11 -0400

    Change bar height

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const bars = document.getElementById('bars');
 
 const today = new Date();
 const barCount = 500;
-const barHeight = 1;
+const barHeight = 5;
 
 for (let i = 0; i < barCount; i++) {
     const date = new Date(today);
@@ -22,6 +22,7 @@ container.addEventListener('scroll', () => {
     }
 });
 
+// Create RGB Bar
 function createBar(date) {
     const rgb = dateToRGB(date);
     const bar = document.createElement('div');
@@ -29,10 +30,26 @@ function createBar(date) {
     bar.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
     bars.appendChild(bar);
 }
+// Create Hex Bar
+// function createBar(date) {
+//     const hex = dateToHex(date);
+//     const bar = document.createElement('div');
+//     bar.className = 'bar';
+//     bar.style.backgroundColor = hex;
+//     bars.appendChild(bar);
+// }
 
+//Convert date to RGB Value
 function dateToRGB(date) {
     const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear() % 100;
     return [month, day, year];
+}
+//Convert date to Hex value
+function dateToHex(date) {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear() % 100;
+    return `#${month}${day}${year}`;
 }
